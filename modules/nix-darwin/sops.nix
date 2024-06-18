8836d3de1c4577b6adef30e40753c7c8ac1db025
Sridhar Ratnakumar <srid@srid.ca>
2024-06-18 17:18:07 -0400

    Revert "disable userMode"
    This reverts commit 187b8215b89971f85b29912a41d87f3d6f85f7c4.

--- modules/nix-darwin/sops.nix
+++ modules/nix-darwin/sops.nix
@@ -98,7 +98,7 @@ let
       sshKeyPaths = cfg.gnupg.sshKeyPaths;
       ageKeyFile = cfg.age.keyFile;
       ageSshKeyPaths = cfg.age.sshKeyPaths;
-      userMode = false;
+      userMode = true;
       logging = {
         keyImport = builtins.elem "keyImport" cfg.log;
         secretChanges = builtins.elem "secretChanges" cfg.log;
